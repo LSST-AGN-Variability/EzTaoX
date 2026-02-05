16bb490453b7a6a72687b9a3b72a00d8927c4d5a
Olivia Lynn <olynn@andrew.cmu.edu>
2026-02-05 13:52:22 -0500

    Add test_downsampleByTime

--- tests/eztaox/test_ts_utils.py
+++ tests/eztaox/test_ts_utils.py
@@ -2,19 +2,34 @@
 
 import numpy as np
 
-from eztaox.ts_utils import formatlc
+from eztaox.ts_utils import downsampleByTime, formatlc
+
+
+def test_downsampleByTime() -> None:  # noqa: N802
+    """Test the time series downsampling utility."""
+
+    tIn = np.array([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
+    tOut = np.array([0.2, 2.7, 4.5])
+
+    # Downsample
+    expected = np.array([0.0, 3.0, 4.0])
+    res = np.array(downsampleByTime(tIn, tOut))
+
+    # Verify output
+    assert np.allclose(expected, res)
 
 
 def test_formatlc() -> None:
     """Test the light curve formatting utility."""
 
-    # Create toy data
     ts, ys, yerrs = {}, {}, {}
     band_order = {"g": 0, "r": 1, "i": 2}
     for band in band_order:
         ts[band] = np.array([1.0, 2.0, 3.0])
         ys[band] = np.array([-0.2, 0.7, 0.1])
         yerrs[band] = np.array([0.08, 0.1, 0.03])
+
+    # Format light curves
     X, y, yerr = formatlc(ts, ys, yerrs, band_order)
 
     # Verify outputs
