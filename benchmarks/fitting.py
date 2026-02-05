855fd212eee390b12bdadce848d289443135e0e2
jli184 <jli184@illinois.edu>
2026-02-05 17:19:44 -0500

    add m32 kernel

--- benchmarks/fitting.py
+++ benchmarks/fitting.py
@@ -3,6 +3,7 @@
 import eztaox.kernels.quasisep as ekq
 import jax
 import jax.numpy as jnp
+import tinygp
 from eztaox.simulator import UniVarSim
 from tinygp import GaussianProcess, kernels
 
@@ -14,15 +15,16 @@ class KernelFittingSuite:
 
     def setup(self, n):
         self.X, self.y, self.diag = generate_data(n)
-        self.drw_params = {
+        self.fitting_params = {
             "log_kernel_param": jnp.log(jnp.array([100, 0.1])),
             "log_amp_delta": jnp.log(0.6),
             "lag": jnp.array(10),
         }
-        self.loss = self._precompile_loss(n)
+        self.loss_exp = self._precompile_exp_loss(n)
+        self.loss_m32 = self._precompile_m32_loss(n)
 
-    def _precompile_loss(self, n):
-        """Precompile the JIT compiled loss function"""
+    def _precompile_exp_loss(self, n):
+        """Precompile the JIT compiled loss function for exponential kernel"""
 
         @jax.jit
         def loss(params):
@@ -36,11 +38,45 @@ class KernelFittingSuite:
             )
             return -gp.log_probability(self.y)
 
-        loss(self.drw_params).block_until_ready()
+        loss(self.fitting_params).block_until_ready()
         return loss
 
-    def time_run_drw_fitting(self, n):
-        self.loss(self.drw_params).block_until_ready()
+    def _precompile_m32_loss(self, n):
+        """Precompile the JIT compiled loss function for Matern32 with lags"""
+
+        log_amps = amp_transform(self.fitting_params)
+        lags = jnp.insert(jnp.atleast_1d(self.fitting_params["lag"]), 0, 0.0)
+
+        t = self.X[0]
+        band = self.X[1]
+
+        new_X, inds = lag_transform(self.X, lags)
+        # t, band = new_X
+
+        @jax.jit
+        def loss(params):
+            kernel = MB(
+                amplitudes=jnp.exp(log_amps),
+                lags=lags,
+                kernel=kernels.quasisep.Matern32(*jnp.exp(params["log_kernel_param"])),
+            )
+            gp = GaussianProcess(
+                kernel,
+                (t[inds], band[inds]),
+                diag=self.diag[inds],
+                mean=0.0,
+                assume_sorted=True,
+            )
+            return -gp.log_probability(self.y)
+
+        loss(self.fitting_params).block_until_ready()
+        return loss
+
+    def time_run_exp_fitting(self, n):
+        self.loss_exp(self.fitting_params).block_until_ready()
+
+    def time_run_m32_fitting(self, n):
+        self.loss_m32(self.fitting_params).block_until_ready()
 
 
 def generate_lc(n, band):
@@ -86,3 +122,27 @@ def generate_data(n):
     y = jnp.concatenate((lc_g, lc_i))[inds]
     diag = jnp.concatenate((yerr_g, yerr_i))[inds] ** 2
     return X, y, diag
+
+
+# GP class
+@tinygp.helpers.dataclass
+class MB(tinygp.kernels.quasisep.Wrapper):
+    amplitudes: jnp.ndarray
+    lags: jnp.ndarray
+
+    def coord_to_sortable(self, X):
+        return X[0]
+
+    def observation_model(self, X):
+        return self.amplitudes[X[1]] * self.kernel.observation_model(X[0])
+
+
+def amp_transform(params):
+    return jnp.insert(jnp.atleast_1d(params["log_amp_delta"]), 0, 0.0)
+
+
+def lag_transform(X, lags):
+    t, band = X
+    new_t = t - lags[band]
+    inds = jnp.argsort(new_t)
+    return (new_t, band), inds
