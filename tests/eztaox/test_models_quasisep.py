28f73658066772b0f5010f53c0d48afd6e5bc8b8
burke86 <colin.burke.j@gmail.com>
2026-02-12 19:11:11 -0500

    cleanup

--- tests/eztaox/test_models_quasisep.py
+++ tests/eztaox/test_models_quasisep.py
@@ -7,8 +7,8 @@ import pytest
 from numpy import random as np_random
 from tinygp import GaussianProcess
 from tinygp.helpers import JAXArray
-from tinygp.test_utils import assert_allclose
 from tinygp.kernels import Exp as Exp_nonqs
+from tinygp.test_utils import assert_allclose
 
 from eztaox.kernels import quasisep
 from eztaox.models import MultiVarModel, UniVarModel
@@ -272,7 +272,7 @@ def test_univar_qs_vs_nonqs_exp_same(data, random):
     # Quasisep kernel
     k_qs = quasisep.Exp(sigma=sigma, scale=scale)
 
-    # Non-quasisep tinygp kernel    
+    # Non-quasisep tinygp kernel
     k_nonqs = sigma**2 * Exp_nonqs(scale=scale)
 
     mean = jnp.array(random.uniform(-1, 1))
